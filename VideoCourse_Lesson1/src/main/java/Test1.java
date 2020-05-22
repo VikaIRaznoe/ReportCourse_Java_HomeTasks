8e6032b55f67ae1f3f6b9304ae6ab0007a26ae39
Vika Ibraimova <ibraimova2711@gmail.com>
2020-05-22 21:55:04 +0300

    deletedepencies juni

--- VideoCourse_Lesson1/src/main/java/Test1.java
+++ VideoCourse_Lesson1/src/main/java/Test1.java
@@ -1,6 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("               РУБАИ                    ");
+        System.out.println("               РУБАИ");
         System.out.println("Много лет размышлял я над жизнью земной.");
         System.out.println("Непонятного нет для меня под луной.     ");
         System.out.println("Мне известно,что мне ничего неизвестно. ");
